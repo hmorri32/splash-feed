6eabf2d538a660765f8125629e3dfba18befd1a7
Hugh Morrison <hmorri32@gmail.com>
2018-02-27 16:57:28 -0500

    renders grid and list buttons depending on route in header.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { NavLink } from "react-router-dom";
+import { Route } from "react-router-dom";
 import grid from "./assets/grid.png";
 import list from "./assets/list.png";
 
@@ -27,23 +28,28 @@ export const Header = ({ active, handleActive }) => {
           Style Guide
         </NavLink>
       </div>
-      <div className="btns">
-        <button
-          className={active === 1 ? "btn grid-btn active" : "btn grid-btn"}
-          onClick={() => handleActive(1)}>
-          <img src={grid} alt="grid-icon" />Grid
-        </button>
-        <button
-          className={active === 2 ? "btn grid-btn active" : "btn grid-btn"}
-          onClick={() => handleActive(2)}>
-          <img src={list} alt="list-icon" />List
-        </button>
-      </div>
+      <Route
+        exact path="/"
+        render={() => (
+          <div className="btns">
+            <button
+              className={active === 1 ? "btn grid-btn active" : "btn grid-btn"}
+              onClick={() => handleActive(1)}>
+              <img src={grid} alt="grid-icon" />Grid
+            </button>
+            <button
+              className={active === 2 ? "btn grid-btn active" : "btn grid-btn"}
+              onClick={() => handleActive(2)}>
+              <img src={list} alt="list-icon" />List
+            </button>
+          </div>
+        )}
+      />
     </header>
   );
 };
 
 Header.propTypes = {
-  active: PropTypes.bool,
+  active: PropTypes.number,
   handleActive: PropTypes.func
 };
