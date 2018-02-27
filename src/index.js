9fe2085c1eddfe8f085e0cae74da4073804f1e6c
Hugh Morrison <hmorri32@gmail.com>
2018-02-27 10:33:58 -0500

    configures react router redux

--- src/index.js
+++ src/index.js
@@ -1,19 +1,32 @@
 import React from "react";
 import { render } from "react-dom";
-import thunk from "redux-thunk";
+
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
-import reducer from "./reducers/RootReducer";
+import thunk from "redux-thunk";
 
+import createHistory from "history/createBrowserHistory";
+import { ConnectedRouter, routerMiddleware } from "react-router-redux";
+import { Route } from "react-router-dom";
+
+import reducer from "./reducers/RootReducer";
 import App from "./components/app/App";
 import registerServiceWorker from "./registerServiceWorker";
 
+const history = createHistory();
+const middleware = routerMiddleware(history);
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-const store = createStore(reducer, devTools, applyMiddleware(thunk));
+const store = createStore(
+  reducer,
+  devTools,
+  applyMiddleware(thunk, middleware)
+);
 
 const index = (
   <Provider store={store}>
-    <App />
+    <ConnectedRouter history={history}>
+      <Route path="/" component={App} />
+    </ConnectedRouter>
   </Provider>
 );
 
