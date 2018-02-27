30d06f1dee925f1cda399f28f9ea74f4aa24c412
Hugh Morrison <hmorri32@gmail.com>
2018-02-27 08:12:48 -0500

    maps dispatch to app

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -1,15 +1,23 @@
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
-import { fetchFeaturedPhotos, storeFeaturedPhotos } from "../actions/feedActions";
+import {
+  fetchFeaturedPhotos,
+  storeFeaturedPhotos,
+  preLoad,
+  generatorFetch
+} from "../actions/feedActions";
 
 const mapStateToProps = state => {
   return {
     featured: state.FeaturedPhotos.photos
-  }
+  };
 };
 
 const mapDispatchToProps = dispatch => {
-  return bindActionCreators({ fetchFeaturedPhotos, storeFeaturedPhotos }, dispatch);
+  return bindActionCreators(
+    { fetchFeaturedPhotos, storeFeaturedPhotos, preLoad, generatorFetch },
+    dispatch
+  );
 };
 
 export default connect(mapStateToProps, mapDispatchToProps);
