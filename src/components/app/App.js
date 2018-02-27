9c5278e14302f68cda29cac4e48ba8a446c24008
Hugh Morrison <hmorri32@gmail.com>
2018-02-27 16:41:03 -0500

    heroku debugging

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -20,6 +20,7 @@ class App extends Component {
   }
 
   componentDidMount() {
+    console.log(process.env);
     this.generator.next();
     const { fetchFeaturedPhotos, storeFeaturedPhotos } = this.props;
     // fetchFeaturedPhotos()
