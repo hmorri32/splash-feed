99d2af8c467ab1f9691a4f64049bb0dd6197b2b4
Hugh Morrison <hmorri32@gmail.com>
2018-02-27 08:12:56 -0500

    generator to handle URL generation

---
+++ src/helpers/helper.js
@@ -0,0 +1,21 @@
+export function* generatePages(total = 100, page = 1) {
+  while (!(page === total)) {
+    yield page;
+    page++;
+  }
+  return 100;
+}
+
+export function* generateUrl() {
+  const generator = generatePages();
+  let { value, done } = generator.next();
+  while (!done) {
+    yield `https://api.unsplash.com/collections/featured/?client_id=${
+      process.env.REACT_APP_SPLASH_ID
+    }&per_page=6&page=${value}`;
+    let { value, done } = generator.next();
+  }
+  return `https://api.unsplash.com/collections/featured/?client_id=${
+    process.env.REACT_APP_SPLASH_ID
+  }&per_page=6&page=${value}`;
+}
