1c2ccfdf6308163a7c61d4e8ece69374cd826539
Hugh Morrison <hmorri32@gmail.com>
2018-03-01 10:10:35 -0500

    adds search actions

--- src/actions/feedActions.js
+++ src/actions/feedActions.js
@@ -14,12 +14,29 @@ export const preLoad = photos => {
   };
 };
 
+export const storeSearchedPhotos = photos => {
+  return {
+    type: "STORE_SEARCHED",
+    data: photos
+  };
+};
+
 export const fetchFeaturedPhotos = () => {
   return dispatch => {
     return fetch(
       `${unsplash}/collections/featured/?client_id=${
         process.env.REACT_APP_SPLASH_ID
       }&per_page=6`
+    );
+  };
+};
+
+export const fetchSearchedPhotos = (query) => {
+  return dispatch => {
+    return fetch(
+      `${unsplash}/search/photos/?client_id=${
+        process.env.REACT_APP_SPLASH_ID
+      }&per_page=6&query=${query}`
     );
   };
 };
