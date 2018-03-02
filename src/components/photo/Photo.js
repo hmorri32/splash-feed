0ba71c8682f89ba348d056d6fbdff284a11a38ab
Hugh Morrison <hmorri32@gmail.com>
2018-03-01 22:06:28 -0500

    builds out photo component

--- src/components/photo/Photo.js
+++ src/components/photo/Photo.js
@@ -1,5 +1,23 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
-export const Photo = ({ i, photo }) => {
-  return <div>cool</div>;
+export const Photo = ({ src, id, title }) => {
+  return (
+    <div className="photo-card">
+      {title ?
+        <Link to={`/photo/${id}`} className="photo-link">
+          <img src={src} alt="featured" />
+          <p>{title}</p>
+        </Link>
+        : <img src={src} alt="featured" /> }
+    </div>
+  );
+};
+
+Photo.propTypes = {
+  src: PropTypes.string,
+  title: PropTypes.string,
+  id: PropTypes.number,
+  photo: PropTypes.object
 };
